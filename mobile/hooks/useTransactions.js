79a48936a5358a0e983deec7cba1135d8cb5e095
kenPriolo <kenpriolo27@gmail.com>
2025-08-18 23:33:49 +0800

    First commit

--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -1,6 +1,7 @@
 // react custom hook file
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { Alert } from "react-native";
 
 const API_URL = "http://localhost:5001/api"
 
@@ -12,4 +13,54 @@ export const useTransactions = (userId) => {
         expenses: 0,
     });
     const [isLoading, setIsLoading] = useState(true);
-}
+
+    // useCallback is used for performance reasons, it will memoize the function
+    const fetchTransactions = useCallback (async () => {
+        try {
+            const response = await fetch(`${API_URL}/transactions/${userId}`);
+            const data = await response.json();
+            setTransactions(data);
+        } catch (error) {
+            console.log("Error fetching transactions:", error);
+        }
+    },[userId]);
+
+    const fetchSummary = useCallback (async () => {
+        try {
+            const response = await fetch(`${API_URL}/transactions/summary/${userId}`);
+            const data = await response.json();
+            setTransactions(data);
+        } catch (error) {
+            console.log("Error fetching transactions:", error);
+        }
+    },[userId]);
+
+    const loadData = useCallback(async () => {
+        if (!userId) return;
+
+        setIsLoading(true);
+
+        try {
+            await Promise.all([fetchTransactions(), fetchSummary()]);
+        } catch (error) {
+            console.log("Error loading data:", error);
+        } finally  {
+            setIsLoading(false);
+        }
+    }, [fetchTransactions, fetchSummary, userId]);
+
+    const deleteTransaction = async (id) => {
+        try {
+            const response = await fetch(`${API_URL}/transactions/${userId}`, { method: "DELETE" });
+
+            if (!response.ok) throw new Error("Failed to delete transaction");
+            loadData();
+            Alert.alert("Success", "Transaction deleted successfully!");
+        } catch (error) {
+            console.log("Error deleting transaction:", error);
+            Alert.alert("Error", error.message);
+        }
+    };
+
+    return { transactions, summary, isLoading, loadData, deleteTransaction};
+};
